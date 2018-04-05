88c52f9a70fd7214c5dfbd93643197f656bbc0e1
Ayrton <asadams89@gmail.com>
2018-04-05 18:16:45 +0200

    Modification de la méthode main afin de prendre en compte les paramètres de lancement via le tableau de la même méthode

--- src/com/main/java/Main.java
+++ src/com/main/java/Main.java
@@ -16,16 +16,15 @@ public class Main {
 	 * <p>L'objet static permettant l'execution de l'application</p>
 	 */
 	public static StartUp start;
-	
-	private static Scanner scan = new Scanner(System.in);
-	
+		
 	/**
 	 * <p>La méthode main de l'application</p>
-	 * <p>Il y a trois possibilité de lancement de l'application où seul l'instanciation de l'objet "start" diffère (mis sous forme de commentaire): 
+	 * 
+	 * <p>Pour lancer cette méthode en modifiant le mode développeur (et passer outre le fichier config.properties) 
+	 * 	l'argument à mettre en paramètre est :
 	 * 	<ul>
-	 * 		<li>"start = new StartUp(true);" => lancement de l'application avec le mode développeur activé</li>
-	 * 		<li>"start = new StartUp(false);" => lancement de l'application avec le mode développeur désactivé</li>
-	 * 		<li>"start = new StartUp();" => lancement de l'application où le mode développeur est définit via le fichier de configuration "config.properties"</li>
+	 * 		<li>pour l'activation du mode développeur -> devMode=enable</li>
+	 * 		<li>pour la désactivation du mode développeur -> devMode=disable</li>
 	 * 	</ul>
 	 * </p>
 	 * 
@@ -36,37 +35,26 @@ public class Main {
 		
 		logMain.info("Lancement de l'application");
 		
-		boolean again = true;
-		int answer;
+		boolean devMode = false;
+		boolean property = false;
 		
-		while(again) {
-			try {
-				
-				System.out.println("Activer le mode développeur ?");
-				System.out.println("1. Oui          2. Non          3. Paramètre par défaut");
-				answer = scan.nextInt();
-			
-				switch(answer) {
-					case 1: 
-						start = new StartUp(true);
-						again = false;
-						break;
-					case 2:
-						start = new StartUp(false);
-						again = false;
-						break;
-					case 3:
-						start = new StartUp();
-						again = false;
-						break;
-					default:
-						logMain.error("Saisie invalide dans le choix du mode développeur => ["+answer+"]");
-					
-				}
-				
-			} catch (InputMismatchException e) {
-				logMain.error("Saisie invalide dans choiceGame() => ["+scan.nextLine()+"]");				
-			} 
+		for(String str : args) {
+			if(str.equals("devMode=enable")) {
+				devMode = true;
+				property = true;
+				break;
+			}
+			else if(str.equals("devMode=disable")) {
+				property = true;
+				break;
+			}
+		}
+		
+		if(property) {
+			start = new StartUp(devMode);
+		}
+		else {
+			start = new StartUp();
 		}
 		
 		start.launch();
